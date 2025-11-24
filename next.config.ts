3a97460fb8429d2722ff1dc44195f7ce3139529f
hosseinalaei <89533880+hosseinalaei@users.noreply.github.com>
2025-11-24 22:04:31 +0330

    fix: add gh-pages conf

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["kimchiapp.info", "simbaapp.info"],
   },
+  output: "export", // important for static export
+  basePath: isProd ? "/hotflix" : "",
+  assetPrefix: isProd ? "/hotflix/" : "",
 };
 
 export default nextConfig;
