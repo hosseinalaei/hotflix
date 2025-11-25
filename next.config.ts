d8927867a047d5ebe2a849f92fd0a3b6dd6b9d97
hosseinalaei <89533880+hosseinalaei@users.noreply.github.com>
2025-11-25 20:57:10 +0330

    feat: downgrade next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["kimchiapp.info", "simbaapp.info"],
   },
