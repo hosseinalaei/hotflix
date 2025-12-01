ae4090425e9ae25e476f418f28a575f0be4931d2
hosseinalaei <89533880+hosseinalaei@users.noreply.github.com>
2025-12-01 18:54:54 +0330

    fix: base url

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import baseAxios from "axios";
 
 const apiConfig = baseAxios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api`,
+  baseURL: `https://movie.one-developer.ir/api`,
   // baseURL: "http://api.themoviedb.org/3/discover/movie?api_key=1e32e196d77987f5d442a50704d6481e",
   // baseURL: "https://murmuring-tundra-31743.herokuapp.com/movies/3/discover/movie?api_key=1e32e196d77987f5d442a50704d6481e",
   // baseURL: "https://murmuring-tundra-31743.herokuapp.com/movies/3",
