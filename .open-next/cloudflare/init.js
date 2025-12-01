ca50fb3ccc0a350a8ad95301c9027e58e04061ad
hosseinalaei <89533880+hosseinalaei@users.noreply.github.com>
2025-12-01 18:28:47 +0330

    fix: cloud next files

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1764486684647,
+    __BUILD_TIMESTAMP_MS__: 1764601032638,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
